ccc3e88d000d1381d906fb82a1210b9d0f5df39c
BTrippleG <116832684+BTrippleG@users.noreply.github.com>
2022-12-05 18:41:31 -0500

    Added controls
    Will need to change line 57 after Gson is introduced

--- src/application/DnDMainController.java
+++ src/application/DnDMainController.java
@@ -1,25 +1,64 @@
 package application;
+
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.layout.HBox;
+import javafx.scene.image.ImageView;
 
 public class DnDMainController {
 
-    @FXML
-    private Label alignment;
+	@FXML
+	private Button button;
 
-    @FXML
-    private HBox name;
+	@FXML
+	private Label charisma;
 
-    @FXML
-    private Label size;
+	@FXML
+	private Label constitution;
 
-    @FXML
-    private Label type;
+	@FXML
+	private Label dexterity;
 
-    //@FXML
-    //void updateLabel(ActionEvent event) {
-    //	name.setText(monsSlug());
-    	
-    //}
+	@FXML
+	private Label intelligents;
+
+	@FXML
+	private Label name;
+
+	@FXML
+	private Label sizeTypeAli;
+
+	@FXML
+	private Label strength;
+
+	@FXML
+	private ImageView typeImage;
+
+	@FXML
+	private Label wisdom;
+
+	@FXML
+	void buttonPressed(ActionEvent event) throws IOException {
+		MonsterMethods info = new MonsterMethods();
+
+		// Set stats
+		strength.setText(info.getStrength());
+		dexterity.setText(info.getDexterity());
+		constitution.setText(info.getConstitution());
+		intelligents.setText(info.getIntelligents());
+		wisdom.setText(info.getWisdom());
+		charisma.setText(info.getCharisma());
+
+		// set name info
+		// typeImage.setImage(new Image("Beast.png"));
+		name.setText(info.monsSlug());
+		sizeTypeAli.setText(info.combineSizeTypeAli());
+
+		// set "scroll pane"
+
+	}
+
 }
