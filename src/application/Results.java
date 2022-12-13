22e10562d07aba6d9c1d43b4fbdd7867abf6504d
BTrippleG <116832684+BTrippleG@users.noreply.github.com>
2022-12-13 18:34:03 -0500

    Update and rename MonsterParser.java to Results.java

---
+++ src/application/Results.java
@@ -0,0 +1,24 @@
+package application;
+
+public class Results {
+
+	public String slug;
+	public String name;
+	public String size;
+	public String type;
+	public String alignment;
+	public String armor_class;
+	public String armor_desc;
+	public String hit_dice;
+	public String damage_resistances;
+	public String damage_immunities;
+	public String hit_points;
+	public String strength;
+	public String dexterity;
+	public String constitution;
+	public String intelligence;
+	public String wisdom;
+	public String charisma;
+	public String perception;
+
+}
