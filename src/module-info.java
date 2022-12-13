297ce9cdb532f314c1c544f8d8cc56825d93c7c3
BTrippleG <116832684+BTrippleG@users.noreply.github.com>
2022-12-13 18:36:29 -0500

    Update module-info.java

--- src/module-info.java
+++ src/module-info.java
@@ -1,8 +1,9 @@
 module DnDMonster {
 	requires javafx.controls;
 	requires javafx.fxml;
-	requires json.simple;
-	requires java.json;
+	requires com.google.gson;
+	requires javafx.graphics;
+	requires java.desktop;
 	
-	opens application to javafx.graphics, javafx.fxml;
+	opens application to javafx.graphics, javafx.fxml, com.google.gson;
 }
